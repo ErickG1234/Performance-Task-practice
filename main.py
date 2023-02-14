e6fd7579f601b3d8ab4e9d9ab346a28678472883
Erick .oG Gutierrez <113930446+ErickG1234@users.noreply.github.com>
2023-02-14 18:59:03 +0000

    So clode

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from other import *
 import math
 import random
 
-dribbling_stats(dribbling_input, sum, final_dribbling, remove_low_number, low_number)
+dribbling_stats(dribbling_input, sum, final_dribbling, low_number)
 remove_low_number(dribbling_input, low_number)
+
 
--- other.py
+++ other.py
@@ -1,23 +1,39 @@
-dribbling_input = list(map(int, input("Enter your player's agility, balance, reactions, ball control, dribbling, and composure in this order (Enter a space between each number): ").split()))
+dribbling_input = list(
+  map(
+    int,
+    input(
+      "Enter your player's agility, balance, reactions, ball control, dribbling, and composure in this order (Enter a space between each number): "
+    ).split()))
 sum = sum(dribbling_input)
 final_dribbling = round(sum / 6)
 
-low_number = 10
+low_number = 9
+
 
 def remove_low_number(dribbling_input, low_number):
-    low_number_found = False
-    for element in dribbling_input:
-        if element < low_number:
-            print("You entered a number less than 10. Try again!")
-            low_number_found = True
-            break
-    return low_number_found
+  low_number_found = False
+  for element in dribbling_input:
+    if element <= low_number:
+      print("You entered a number less than 10. Try again!")
+      low_number_found = True
+      break
+  return low_number_found
+
 
 def dribbling_stats(dribbling_input, sum, final_dribbling, low_number):
-    print("List of stats: ", dribbling_input)
-    low_number_found = remove_low_number(dribbling_input, low_number)
-    if low_number_found:
-        dribbling_input = list(map(int, input("Enter your player's agility, balance, reactions, ball control, dribbling, and composure in this order (Enter a space between each number): ").split()))
-        dribbling_stats(dribbling_input, sum, final_dribbling, low_number)
-    else:
-        print(f"Your player's final dribbling stat: {final_dribbling} ")
+  print("List of stats: ", dribbling_input)
+  low_number_found = remove_low_number(dribbling_input, low_number)
+  if low_number_found:
+    dribbling_input = list(
+      map(
+        int,
+        input(
+          "Enter your player's agility, balance, reactions, ball control, dribbling, and composure in this order (Enter a space between each number): "
+        ).split()))
+    print(sum)
+    print(final_dribbling)
+    print(f"Your player's final dribbling stat: {final_dribbling} ")
+  else:
+    print(f"Your player's final dribbling stat: {final_dribbling} ")
+
+  # Maybe try to transfer new data in a new list for that
